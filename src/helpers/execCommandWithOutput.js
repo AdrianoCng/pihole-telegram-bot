406626cd8446739f1442323e7cb0499d3e6668eb
Adriano Cangiamila <adriano.cangiamila@tain.com>
2025-11-08 21:20:22 +0000

    Enhance error handling in execCommandWithOutput to reject on non-zero exit codes

--- src/helpers/execCommandWithOutput.js
+++ src/helpers/execCommandWithOutput.js
@@ -18,11 +18,16 @@ export default function execCommandWithOutput(ctx, command, args = []) {
 
     process.stderr.on("data", (chunk) => {
       sendMessage(ctx, chunk.toString());
-      reject(chunk.toString());
     });
 
-    process.on("close", () => {
-      resolve();
+    process.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        const errorMessage = `Command failed with exit code ${code}`;
+        sendMessage(ctx, errorMessage);
+        reject(new Error(errorMessage));
+      }
     });
   });
 }
